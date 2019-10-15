22a70e6bc1d471a34bbf31d8c0a818609f7bb1e3
nancyho629 <nancyho629@gmail.com>
2019-10-15 08:51:04 -0400

    Add alert messaging

--- src/components/AutoDismissAlert/messages.js
+++ src/components/AutoDismissAlert/messages.js
@@ -5,5 +5,13 @@ export default {
   signInFailure: 'Failed to sign in. Check your email and password and try again.',
   signOutSuccess: 'Come back soon!',
   changePasswordSuccess: 'Password changed successfully!',
-  changePasswordFailure: 'Failed to change passwords. Check your old password and try again.'
+  changePasswordFailure: 'Failed to change passwords. Check your old password and try again.',
+  deleteFailure: 'Failed to delete!',
+  deleteSuccess: 'Successfully deleted!',
+  createFailure: 'Failed to create a new trip!',
+  createSuccess: 'Successfully created a new trip!',
+  getFailure: 'Failed to get all the trips!',
+  getSuccess: 'Successfully got all the trips!',
+  showFailure: 'Failed to get the trip!',
+  updateSuccess: 'Successfully updated the trip!'
 }
