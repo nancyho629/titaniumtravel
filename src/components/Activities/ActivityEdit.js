a1a0ba41ad8a802af8da48f3247bcde3461bd2bf
nancyho629 <nancyho629@gmail.com>
2019-10-17 12:35:56 -0400

    Add ActivityEdit component

---
+++ src/components/Activities/ActivityEdit.js
@@ -0,0 +1,67 @@
+import React, { useState, useEffect } from 'react'
+import axios from 'axios'
+import apiUrl from '../../apiConfig'
+import ActivityForm from './ActivityForm'
+import { withRouter } from 'react-router-dom'
+// import messages from '../AutoDismissAlert/messages'
+
+const ActivityEdit = ({ user, alert, match, history }) => {
+  const [activity, setActivity] = useState({ activity: '', date: '', description: '' })
+
+  useEffect(() => {
+    axios({
+      method: 'GET',
+      url: `${apiUrl}/trips/${match.params.id}/activities/${match.params.aid}`,
+      headers: {
+        'Authorization': `Token token=${user.token}` // `Bearer ${user.token}`
+      }
+    })
+      .then(res => {
+        let formattedDate = ''
+        if (res.data.activity.date) {
+          const dateObj = new Date(res.data.activity.date)
+          formattedDate = dateObj.toISOString().substring(0, 10)
+        }
+        setActivity({
+          ...res.data.activity, date: formattedDate
+        })
+      })
+      .catch(console.error)
+  }, [])
+
+  const handleChange = event => {
+    event.persist()
+    setActivity(activity => ({ ...activity, [event.target.name]: event.target.value }))
+  }
+
+  const handleSubmit = event => {
+    event.preventDefault()
+
+    axios({
+      url: `${apiUrl}/trips/${match.params.id}/activities/${match.params.aid}`,
+      method: 'PATCH',
+      headers: {
+        'Authorization': `Bearer ${user.token}`
+      },
+      data: { activity }
+    })
+      .then(() => alert({ heading: 'Success', message: 'You updated a trip!', variant: 'success' }))
+      .then(() => history.push(`/trips/${match.params.id}/activities/${match.params.aid}`))
+      // .then(() => alert({
+      //   heading: 'Update Success',
+      //   message: messages.updateSuccess,
+      //   variant: 'success'
+      // }))
+      .catch(() => alert({ heading: 'Danger', message: 'You did not update a trip!', variant: 'danger' }))
+  }
+
+  return (
+    <ActivityForm
+      activity={activity}
+      handleChange={handleChange}
+      handleSubmit={handleSubmit}
+    />
+  )
+}
+
+export default withRouter(ActivityEdit)
